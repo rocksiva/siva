aacd83b51fd8861797d0acbcb2839f3e264bc908
rocksiva <42169851+rocksiva@users.noreply.github.com>
2018-08-07 14:28:35 +0530

    Create positive.java

---
+++ positive.java
@@ -0,0 +1,16 @@
+class aniruth
+{
+public static void main(String args[])
+  {
+  int num;
+  Scanner sc new Scanner(System.in);
+  System.out.println("enter the number");
+  num=sc.nextInt();
+  if(num>0)
+    System.out.println("positive");
+  elseif(num<0)
+    System.out.println("negative");
+  else
+    System.out.println("zero");
+    }
+  }
